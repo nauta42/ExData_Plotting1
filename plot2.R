0370dd9d70e50d5f9a77ed98560b3f7a893d74b2
nauta42 <nauta42@gmail.com>
2014-12-08 01:15:21 +0100

    plots 1, 2

--- plot2.R
+++ plot2.R
@@ -1,5 +1,6 @@
 data <- read.table(file = "data.csv", header = TRUE, sep = ";")
 data[, "DateTime"] <- as.POSIXct(paste(data$Date,data$Time), format = "%d/%m/%Y %H:%M:%S")
-hist(data[[3]], col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
-dev.copy(png, file = "plot1.png", width = 480, height = 480)
+plot(y = data[[3]], x = data$DateTime, xlab="",ylab = "Global Active Power (kilowatts)", type = "l")
+
+dev.copy(png, file = "plot2.png", width = 480, height = 480)
 dev.off()
