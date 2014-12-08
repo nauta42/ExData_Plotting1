33bffeec02711728f10e500db1c13f7ff30d79c7
nauta42 <nauta42@gmail.com>
2014-12-08 02:00:36 +0100

    plot 3

--- plot3.R
+++ plot3.R
@@ -1,8 +1,10 @@
 data <- read.table(file = "data.csv", header = TRUE, sep = ";")
 data[, "DateTime"] <- as.POSIXct(paste(data$Date,data$Time), format = "%d/%m/%Y %H:%M:%S")
-plot(data[[7]], x = data$DateTime, xlab="",ylab = "Energy sub metering", type = "l")
-plot(data[[8]], x = data$DateTime, xlab="",ylab = "Energy sub metering", type = "l")
-plot(data[[9]], x = data$DateTime, xlab="",ylab = "Energy sub metering", type = "l")
-#legend("Sub_metering_1", col="black")
+
+plot(data[[7]], x = data$DateTime, xlab="",ylab = "Energy sub metering", type = "l", col = "black")
+lines(data[[8]], x = data$DateTime, col = "red")
+lines(data[[9]], x = data$DateTime, col = "blue")
+legend("topright", lty = 1 , col = c("black","red","blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
 dev.copy(png, file = "plot3.png", width = 480, height = 480)
 dev.off()
