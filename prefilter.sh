aa1f422c18da990d2360f2979db256d03b061bfa
nauta42 <nauta42@gmail.com>
2015-03-08 22:54:24 +0100

    Comments for the prefilter script

--- prefilter.sh
+++ prefilter.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Create a new file for the data with the header line
 head -n1 household_power_consumption.txt > data.csv
-grep '^1/2/2007' household_power_consumption.txt >> data.csv
-grep '^2/2/2007' household_power_consumption.txt >> data.csv
+
+# Append only data from the dates 2007-02-01 and 2007-02-02
+grep '^[12]/2/2007' household_power_consumption.txt >> data.csv
 
